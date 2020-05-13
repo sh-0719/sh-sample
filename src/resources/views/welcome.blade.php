d0fab7657b6d553ec988e554658efe4062327395
Hagiya Shigetsugu <s-hagiya@exrise.co.jp>
2020-05-13 22:14:37 +0900

    トップページに文言追加

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -64,6 +64,7 @@
         </style>
     </head>
     <body>
+    あさい
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
