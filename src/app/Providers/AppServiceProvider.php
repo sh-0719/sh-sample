4ea1adbb6af08dc9475146c7f161eba30a614403
Hagiya Shigetsugu <s-hagiya@exrise.co.jp>
2020-05-13 23:21:29 +0900

    マイグレーション時のvarchar型がMySQL5.7.7未満の最大767bytesに治るように対応

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // MySQL5.7.7未満の最大767bytesに治るように対応
+        Schema::defaultStringLength(191);
     }
 }
