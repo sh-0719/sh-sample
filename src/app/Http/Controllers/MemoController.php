88333852f574243676969c6f23fedfd41136ae8e
Hagiya Shigetsugu <s-hagiya@exrise.co.jp>
2020-05-21 10:23:58 +0900

    ユーザのメモ表示機能を作成

--- src/app/Http/Controllers/MemoController.php
+++ src/app/Http/Controllers/MemoController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Eloquents\Memo;
 use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
     public function index()
     {
-        return view('memo.index', []);
+        $visitorId = \Auth::user()->id;
+        $visitorMemos = Memo::where('user_id', $visitorId)->get();
+        return view('memo.index', ['memos' => $visitorMemos]);
     }
 }
+//$memo = new Memo();
+//$memo->create([
+//    'user_id' => \Auth::user()->id,
+//    'content' => 'てすと'
+//]);
--- src/resources/views/memo/index.blade.php
+++ src/resources/views/memo/index.blade.php
@@ -1,2 +1,21 @@
-<?php
-echo 'てすと';
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+{{ \Auth::user()->name }}さんのメモ一覧です。
+<table>
+    <tr>
+        <th>内容</th>
+        <th>作成日時</th>
+    </tr>
+    @foreach($memos as $memo)
+        <tr>
+            <td>{{ $memo->content }}</td>
+            <td>{{ $memo->created_at }}</td>
+        </tr>
+    @endforeach
+</table>
+
+</body>
+</html>
