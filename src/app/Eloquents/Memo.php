8075964484f36e92e56475cafc837211355ee69f
Hagiya Shigetsugu <s-hagiya@exrise.co.jp>
2020-05-24 17:11:56 +0900

    PHPDocの漏れ追加。MemoにUserへの関係追加

--- src/app/Eloquents/Memo.php
+++ src/app/Eloquents/Memo.php
@@ -2,6 +2,7 @@
 
 namespace App\Eloquents;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -9,15 +10,18 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * Class Memo
  * @package App\Eloquents
  *
- * @property $id
- * @property $user_id
- * @property $content
+ * @property int         $id
+ * @property int         $user_id
+ * @property string|null $content
+ *
+ * @property-read User $user
  */
 class Memo extends Model
 {
     use SoftDeletes;
 
-    // mysql5.5対応。複数カラムのdefaultにCURRENT_TIMESTAMPが使えない
+    // mysql5.5対応（created_ad）
+    // 複数カラムのdefaultにCURRENT_TIMESTAMPが使えない
     /**
      * 変更可能領域
      * @var array
@@ -25,4 +29,13 @@ class Memo extends Model
     protected $fillable = [
         'user_id', 'content', 'created_at'
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * relation: users
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
